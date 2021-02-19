843303162d50383d1d24795b7b0b55d023c8d59e
Andreas Kohn <andreas.kohn@gmail.com>
2021-02-19 12:05:13 +0100

    Synchronize the eslint configuration with the current best practices
    This brings in better defaults for TypeScript, disables TypeScript-specific rules for .js files, and
    enforces prettier standards.
    
    Note that this is still "work-in-progress": Prettier support likely needs to be come conditional on
    whether there is a .prettierrc or other configuration available.

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
+const { existsSync } = require('fs');
+const { resolve: resolvePath } = require('path');
+
+// Some typescript rules require a configuration to exist
+const tsConfigPath = resolvePath('tsconfig.json');
+const hasTsConfig = existsSync(tsConfigPath);
+
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint'
-	],
 	extends: [
-		'eslint:recommended',
+		'standard',
+		'plugin:promise/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended',
+		'prettier/standard',
+		'prettier/@typescript-eslint'
 	],
+	env: {
+		node: true
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		ecmaFeatures: {
+			legacyDecorators: true,
+			jsx: false
+		}
+	},
+	plugins: [
+		'import',
+		'@typescript-eslint',
+		'disable'
+	],
+	processor: 'disable/disable',
 	rules: {
-		'@typescript-eslint/restrict-template-expressions': 'off',
-		'@typescript-eslint/require-await': 'off',
-	}
+		'space-before-function-paren': 0,
+		'import/export': 0,
+		'no-void': ['error', { allowAsStatement: true }],
+		'require-await': 'off'
+	},
+
+	overrides: [
+		{
+			files: '**/*.ts',
+
+			parserOptions: {
+				project: hasTsConfig ? 'tsconfig.json' : undefined,
+			},
+
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/no-empty-interface': 'off',
+				'no-unused-vars': 'off',
+				'@typescript-eslint/no-unused-vars': ['error'],
+				'no-use-before-define': 'off',
+				'@typescript-eslint/no-use-before-define': ['error'],
+				'no-useless-constructor': 'off',
+				'@typescript-eslint/no-useless-constructor': ['error'],
+
+				'@typescript-eslint/no-floating-promises': hasTsConfig ? ['error', { ignoreVoid: true }] : 'off',
+				'@typescript-eslint/await-thenable': hasTsConfig ? ['error'] : 'off',
+
+				'@typescript-eslint/restrict-template-expressions': 'off',
+				'@typescript-eslint/require-await': 'off'
+			}
+		},
+		{
+			files: '**/*.js',
+
+			settings: {
+				'disable/plugins': [
+					'@typescript-eslint'
+				]
+			},
+
+			rules: {
+				'no-unused-vars': 'error',
+				'no-use-before-define': 'error',
+				'no-useless-constructor': 'error'
+			}
+		}
+	]
 };
